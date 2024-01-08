232426a9e4ee92a0cc4eab836906ce87283323e4
Mutzus <ssmutzus@gmail.com>
2024-01-07 21:25:46 -0800

    Fixing Task1

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
+#include <unistd.h>
+
 int _putchar(char c);
 int _puts(char *str);
+int _printf(const char *format, ...);
+int _print_number(int n);
 
 #endif /* MAIN_H */
